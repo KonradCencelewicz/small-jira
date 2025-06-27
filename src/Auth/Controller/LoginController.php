c82b03f7fc1d84a4944ec9f092aec90d52b60fb1
Konrad Cencelewicz <konrad.cencelewicz@gmail.com>
2025-06-27 14:37:19 +0200

    [AUTH] add logic to redirect user if they are logged in but they try go to /login page

--- src/Auth/Controller/LoginController.php
+++ src/Auth/Controller/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Auth\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -11,9 +12,14 @@ final class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
     public function index(
-        AuthenticationUtils $utils
+        AuthenticationUtils $utils,
+        Security $security
     ): Response
     {
+        if ($security->getUser()) {
+            return $this->redirectToRoute('app_welcome');
+        }
+
         $lastUsername = $utils->getLastUsername();
         $error = $utils->getLastAuthenticationError();
 
