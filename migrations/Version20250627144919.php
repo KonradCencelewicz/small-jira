31f55c74bb961c32194463749e16793e7929b77c
Konrad Cencelewicz <konrad.cencelewicz@gmail.com>
2025-06-27 18:49:50 +0200

    [TASKS] added migration for status, task

---
+++ migrations/Version20250627144919.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250627144919 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE status (id INT AUTO_INCREMENT NOT NULL, label VARCHAR(50) NOT NULL, sequence INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE task (id INT AUTO_INCREMENT NOT NULL, status_id INT NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(4000) DEFAULT NULL, deadline DATETIME DEFAULT NULL, INDEX IDX_527EDB256BF700BD (status_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE task ADD CONSTRAINT FK_527EDB256BF700BD FOREIGN KEY (status_id) REFERENCES status (id)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE task DROP FOREIGN KEY FK_527EDB256BF700BD
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE status
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE task
+        SQL);
+    }
+}
