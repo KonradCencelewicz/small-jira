73552da87bfbb74fdf6783826330572dc9e802b3
Konrad Cencelewicz <konrad.cencelewicz@gmail.com>
2025-06-28 21:50:37 +0200

    [TASKS] refactored TaskService to use Entity

--- src/Tasks/Service/TaskService.php
+++ src/Tasks/Service/TaskService.php
@@ -5,14 +5,17 @@ namespace App\Tasks\Service;
 use App\Tasks\Dto\TaskDto;
 use App\Tasks\Entity\Task;
 use App\Tasks\Dto\TaskCreateDto;
+use App\Tasks\Dto\TaskWithStatusDto;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Tasks\Service\TaskServiceInterface;
 use App\Tasks\Repository\StatusRepositoryInterface;
+use App\Tasks\Repository\TaskRepositoryInterface;
 
 final class TaskService implements TaskServiceInterface
 {
     public function __construct(
         private EntityManagerInterface $em,
+        private TaskRepositoryInterface $taskRepository,
         private StatusRepositoryInterface $statusRepository,
     ) {}
 
@@ -29,5 +32,19 @@ final class TaskService implements TaskServiceInterface
         $this->em->flush();
 
         return TaskDto::fromEntity($task);
+    }
+
+
+    public function updateTaskStatus(int $taskId, int $statusId): TaskWithStatusDto
+    {
+        $task = $this->taskRepository->findOneByIdWithStatus($taskId);
+        $status = $this->statusRepository->findOneById($statusId);
+
+        $task->setStatus($status);
+
+        $this->em->persist($task);
+        $this->em->flush();
+
+        return TaskWithStatusDto::fromEntity($task);
     }
 }
--- src/Tasks/Service/TaskServiceInterface.php
+++ src/Tasks/Service/TaskServiceInterface.php
@@ -4,8 +4,10 @@ namespace App\Tasks\Service;
 
 use App\Tasks\Dto\TaskCreateDto;
 use App\Tasks\Dto\TaskDto;
+use App\Tasks\Dto\TaskWithStatusDto;
 
 interface TaskServiceInterface
 {
     public function createTask(TaskCreateDto $task): TaskDto;
+    public function updateTaskStatus(int $taskId, int $statusId): TaskWithStatusDto;
 }
