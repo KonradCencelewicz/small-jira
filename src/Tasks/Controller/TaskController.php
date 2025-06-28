0d61df76c94c948f52ff1801e88f32a5723b0ba6
Konrad Cencelewicz <konrad.cencelewicz@gmail.com>
2025-06-28 06:04:24 +0200

    [TASKS] added TaskController + set up

---
+++ src/Tasks/Controller/TaskController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Tasks\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Tasks\Repository\StatusRepositoryInterface;
+use App\Tasks\Repository\TaskRepositoryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class TaskController extends AbstractController
+{
+    #[Route('/task/dashboard', name: 'app_task_dashboard')]
+    public function index(
+        TaskRepositoryInterface $taskRepository, 
+        StatusRepositoryInterface $statusRepository
+    ): Response
+    {
+
+        return $this->render(
+            'Tasks/tasks_dashboard/index.html.twig',
+            [
+                'tasks' => $taskRepository->allWithStatus(),
+                'statuses' => $statusRepository->all(),
+            ]
+        );
+    }
+
+    #[Route('/task/create', name: 'app_task_create')]
+    public function create(): Response
+    {
+        return $this->render('Tasks/tasks_dashboard/index.html.twig');
+    }
+}
